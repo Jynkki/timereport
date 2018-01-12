522c5ef628622963fcd65691781ece9439119d1c
Peter Jungner <Peter.Jungner@ericsson.com>
2018-01-12 19:39:13 +0000

    Security upgrades

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(favicon());
 // Uncomment next line if HTTP logging is needed
 //app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
